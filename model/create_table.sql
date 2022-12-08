909d39882e31ce20554c9aac6df6926299980338
heqingbao <qingbao.ho@gmail.com>
2022-12-08 23:40:39 +0800

    update

--- model/create_table.sql
+++ model/create_table.sql
@@ -11,3 +11,21 @@ CREATE TABLE `user` (
     UNIQUE KEY `idx_username` (`username`) USING BTREE,
     UNIQUE KEY `idx_user_id` (`user_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DROP TABLE IF EXISTS `community`;
+CREATE TABLE `community` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `community_id` int(10) UNSIGNED NOT NULL,
+    `community_name` varchar(128) COLLATE utf8mb4_general_ci NOT NULL,
+    `introduction` varchar(256) COLLATE utf8mb4_general_ci NOT NULL,
+    `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_community_id` (`community_id`),
+    UNIQUE KEY `idx_community_name` (`community_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `community` VALUES ('1', '1', 'Go', 'Golang', '2006-01-02 15:04:05', '2006-01-02 15:04:05');
+INSERT INTO `community` VALUES ('2', '2', 'Java', '什么都能做', '2022-01-02 15:04:05', '2022-01-02 15:04:05');
+INSERT INTO `community` VALUES ('3', '3', 'PHP', '世界上最好的语言', '2009-01-02 15:04:05', '2009-01-02 15:04:05');
+INSERT INTO `community` VALUES ('4', '4', 'LOL', '欢迎来到英雄联盟', '2016-12-01 07:34:05', '2016-12-01 07:34:05');
